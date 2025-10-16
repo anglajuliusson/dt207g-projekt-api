eab1d013c70535218bd3237f16305c59e795dd7a
Ängla Juliusson <angla02@icloud.com>
2025-10-16 16:14:54 +0200

    Registrering med validering av input

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,29 @@ const router = express.Router();
 // Registrera ny användare
 router.post("/register", async (req, res) => {
     console.log("Register called...");
+
+    try {
+        let { username, password } = req.body;
+    
+        // Validera input, kolla vilka fält som saknas
+        const missingFields = [];
+        if (!username) missingFields.push("username"); // Om användarnamn saknas
+        if (!password) missingFields.push("password"); // Om lösenord saknas
+
+        // Skriv ut det saknade värdet
+        if (missingFields.length > 0) {
+            return res.status(400).json({ error: `Invalid input, send ${missingFields.join(", ")}`}); // Felmeddelande om alla fält inte är ifyllda
+        }
+    
+        // Correct - save user
+        const user = new User ({ username, password });
+        await user.save(); // Spara användare
+    
+        res.status(201).json({ message: "User created"});
+
+    } catch (erro) {
+        res.status(500).json({error: "Server error"});
+    }
 });
 
 // Logga in användare
