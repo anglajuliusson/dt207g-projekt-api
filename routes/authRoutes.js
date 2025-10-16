2e886abeb6abe60a7e6ddbf819ae467e942994e7
Ängla Juliusson <angla02@icloud.com>
2025-10-16 16:46:35 +0200

    Fixa registrering och skapa login med validering av indata och enkel autentiseringskontroll

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) => {
 
     try {
         let { username, password } = req.body;
-    
+
         // Validera input, kolla vilka fält som saknas
         const missingFields = [];
         if (!username) missingFields.push("username"); // Om användarnamn saknas
@@ -19,21 +19,42 @@ router.post("/register", async (req, res) => {
         if (missingFields.length > 0) {
             return res.status(400).json({ error: `Invalid input, send ${missingFields.join(", ")}`}); // Felmeddelande om alla fält inte är ifyllda
         }
-    
+
         // Correct - save user
-        const user = new User ({ username, password });
-        await user.save(); // Spara användare
-    
         res.status(201).json({ message: "User created"});
 
-    } catch (erro) {
-        res.status(500).json({error: "Server error"});
+    } catch (error) {
+        res.status(500).json({ error: "Server error" + error}); // Felmeddelande
     }
 });
 
 // Logga in användare
 router.post("/login", async (req, res) => {
     console.log("Login called...");
+
+    try {
+        let { username, password } = req.body;
+
+        // Validera input, kolla vilka fält som saknas
+        const missingFields = [];
+        if (!username) missingFields.push("username"); // Om användarnamn saknas
+        if (!password) missingFields.push("password"); // Om lösenord saknas
+
+        // Skriv ut det saknade värdet
+        if (missingFields.length > 0) {
+            return res.status(400).json({ error: `Invalid input, send ${missingFields.join(", ")}`}); // Felmeddelande om alla fält inte är ifyllda
+        }
+
+        // Kontroller att username och password är rätt
+        if (username === "user" && password === "password") {
+            res.status(200).json({ message: "Login successful" });
+        } else {
+            res.status(401).json({ error: "Invalid username or password"}); // Felmeddelande om fel användarnamn eller lösenrod
+        }
+
+    } catch(error) {
+        res.status(500).json({ error: "Server error" + error}); // Felmeddelande
+    }
 });
 
 module.exports = router;
