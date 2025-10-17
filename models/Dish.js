ec4dcbb3956e4b660ca671802b504ef1cd56e53b
Ängla Juliusson <angla02@icloud.com>
2025-10-17 10:09:34 +0200

    Dish Schema

---
+++ models/Dish.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); // Inkludera mongoose
+
+// Dish Schema
+const dishSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.new
+  },
+});
+
+// Skapar en Mongoose-modell "Dish" baserat på dishSchema
+const Dish = mongoose.model("Dish", dishSchema); // Modellen används för att interagera med MongoDB (CRUD-operationer)
+module.exports = Dish; // Exporterar modellen så att den kan importeras och användas i andra filer
